1f16cefa4c93bb52a29295cc0b77bd489a3fab1c
amirsantoso <amir.santosos@gmail.com>
2021-09-30 18:20:32 +0700

    Update koneksi.php
    Penambahan Database

--- column/koneksi.php
+++ column/koneksi.php
@@ -1,5 +1,5 @@
 <?php
 include ('parser-php-version.php');
-	mysql_connect("localhost","root","A177irs1m918@123") or die ("Koneksi Gagal");
-	mysql_select_db("db_harusjadi") or die ("Database Tidak Terakses");
+	mysql_connect("eu-cdbr-west-01.cleardb.com","b08b36868116a6","c7b82c31") or die ("Koneksi Gagal");
+	mysql_select_db("heroku_f686e8cfe62ca31") or die ("Database Tidak Terakses");
 ?>
